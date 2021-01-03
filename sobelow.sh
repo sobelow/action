55b8e449886501b66e8e15c30190beab897a526e
GriffinMB <byatt.griffin@gmail.com>
2021-01-03 17:28:34 -0600

    Optional report upload

--- sobelow.sh
+++ sobelow.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 
+REPORT=""
+if [[ $2 == "true" ]]; then
+    REPORT=">> results.sarif"
+fi
+
 mix local.hex --force
 mix escript.install github nccgroup/sobelow --force
 
-~/.mix/escripts/sobelow $1 --format sarif >> results.sarif
+~/.mix/escripts/sobelow $1 --format sarif $REPORT
