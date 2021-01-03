dab7c8c9020a06b756dc82f51d6d82002ba072f8
GriffinMB <byatt.griffin@gmail.com>
2021-01-03 17:51:33 -0600

    Condition around command

--- sobelow.sh
+++ sobelow.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 
-REPORT="--format sarif >> results.sarif"
-if [ "$2" = "false" ]; then
-    REPORT=""
-fi
-
 mix local.hex --force
 mix escript.install github nccgroup/sobelow --force
 
-~/.mix/escripts/sobelow $1 $REPORT
+if [ "$2" = "false" ]; then
+    ~/.mix/escripts/sobelow $1
+else
+    ~/.mix/escripts/sobelow $1 --format sarif >> results.sarif
+fi
