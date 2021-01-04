1afd6d2cae70ae8bd900b58506f54487ed863912
GriffinMB <byatt.griffin@gmail.com>
2021-01-03 18:01:33 -0600

    Basic Sobelow action with sarif file output

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Sobelow Action
 
-...
+
--- sobelow.sh
+++ sobelow.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -l
 
 mix local.hex --force
-mix escript.install github nccgroup/sobelow --force
+mix escript.install hex sobelow --force
 
-~/.mix/escripts/sobelow $1
+if [ "$2" = "false" ]; then
+    ~/.mix/escripts/sobelow $1
+else
+    ~/.mix/escripts/sobelow $1 --format sarif >> results.sarif
+fi
